60639c1954f62f38e111caf14cc512ee17738c2a
Eli <elize.san10@gmail.com>
2021-04-01 12:53:55 -0500

    heroku push

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,46 @@
+// module.exports = {
+//   development: {
+//     client: "pg",
+//     connection: {
+//       host: "localhost",
+//       port: 5000,
+//       database: "database_cookbook",
+//       user: "postgres",
+//       password: "NovaDee3189!",
+//     },
+//     migrations: {
+//       directory: "./api/data/migrations",
+//     },
+//     seeds: {
+//       directory: "./api/data/seeds",
+//     },
+//   },
+// };
+
+const pg = require("pg");
+
+if (process.env.DATABASE_URL) {
+  pg.defaults.ssl = { rejectUnauthorized: false };
+}
+
+const sharedConfig = {
+  client: "pg",
+  migrations: { directory: "./api/data/migrations" },
+  seeds: { directory: "./api/data/seeds" },
+};
 
 module.exports = {
   development: {
-    client: "pg",
-    connection: {
-      host: "localhost",
-      port: 5000,
-      database: "database_cookbook",
-      user: "postgres",
-      password: "NovaDee3189!",
-    },
-    migrations: {
-      directory: "./api/data/migrations",
-    },
-    seeds: {
-      directory: "./api/data/seeds",
-    },
+    ...sharedConfig,
+    connection: process.env.DEV_DATABASE_URL,
+  },
+  testing: {
+    ...sharedConfig,
+    connection: process.env.TESTING_DATABASE_URL,
+  },
+  production: {
+    ...sharedConfig,
+    connection: process.env.DATABASE_URL,
+    pool: { min: 2, max: 10 },
   },
 };
-
-// const pg = require("pg");
-
-// if (process.env.DATABASE_URL) {
-//   pg.defaults.ssl = { rejectUnauthorized: false };
-// }
-
-// const sharedConfig = {
-//   client: "pg",
-//   migrations: { directory: "./api/data/migrations" },
-//   seeds: { directory: "./api/data/seeds" },
-// };
-
-// module.exports = {
-//   development: {
-//     ...sharedConfig,
-//     connection: process.env.DEV_DATABASE_URL,
-//   },
-//   testing: {
-//     ...sharedConfig,
-//     connection: process.env.TESTING_DATABASE_URL,
-//   },
-//   production: {
-//     ...sharedConfig,
-//     connection: process.env.DATABASE_URL,
-//     pool: { min: 2, max: 10 },
-//   },
-// };
